cc5ecb5d7f9efbab0e7aac8757ef486157e3a6c9
Manuel Hernandez <manueldonato9921@gmail.com>
2025-02-26 21:18:27 -0700

    se hizo una clase main, un package de Nodos donde van los nodos simples y nodos dobles, otro package  Lineales donde hay dos clases, una Lista enlasada simple y otra lista doblemente enlazada

--- src/Lineales/LES.java
+++ src/Lineales/LES.java
@@ -1,11 +1,109 @@
 
 package Lineales;
 
+import Nodos.Nodo;
+
 /**
  *
  * @author manue
  */
 public class LES {
+    
+    private Nodo inicial;
+    private int tam;
+    
+    /**
+     * este metodo verifica si la lista está (true) vacía o no (false)
+     * @return  true si "inicial# es nulo, false en caso de que haya al menos un 
+     * nodo en "inicial"
+     */
+    public boolean isEmpty(){
+        if (inicial == null) {
+            return true;
+        }
+        return false;
+    }
+    
+    public int size(){
+        return tam;
+    }
+    
+    /**
+     * Añade elementos al principio de la lista 
+     * @return 
+     */
+    public boolean addFirst(Nodo nN){
+        if (this.isEmpty()) {
+            inicial = nN;
+            tam ++;
+            return true;
+        }else if (size() > 0){
+            nN.setSiguiente(inicial);
+            inicial = nN;
+            tam ++;
+            return true;
+        }
+        return false;
+    }
+    
+     public boolean addLast(int val){
+        //crear el nuevo nodo
+        Nodo nN = new Nodo(val);
+        
+        if (tam == 0) {
+            inicial = nN;
+            tam ++;
+            return true;
+        } else {
+            Nodo actual = inicial;
+            while (actual.getSiguiente() != null) {
+                actual = actual.getSiguiente();
+            }
+            actual.setSiguiente(nN);
+            tam ++;
+            return true;
+        }
+    }
+    
+    public void imprimirListaConsola(){
+        Nodo nActual = inicial;
+        while (nActual != null) {
+            System.out.print(nActual.getValor() + "--> ");           
+                nActual = nActual.getSiguiente();
+        }
+    }
+    
+    public boolean removeFirst(){
+        if (size() == 1) {
+            inicial = null;
+            tam = 0;
+            return true;
+        } else if (size() > 1) {
+            inicial = inicial.getSiguiente();
+            tam --;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public boolean removeLast(){
+        if (size() == 1) {
+            inicial = null;
+            tam = 0;
+            return true;
+        } else if (size() > 1) {
+            Nodo nAux = inicial;
+            while (nAux.getSiguiente().getSiguiente() != null) {
+                nAux = nAux.getSiguiente();
+            }
+            nAux.setSiguiente(null);
+            tam --;
+            return true;
+        } else {
+            return false;
+        }
+    }
     
     
     
